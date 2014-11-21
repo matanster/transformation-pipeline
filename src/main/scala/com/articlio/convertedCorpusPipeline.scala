b1d4056ec99532c1a7a29fac3f4448e7d9534fb3
matanster <dev.matan@gmail.com>
2014-11-21 12:18:01 +0200

    ""

--- src/main/scala/com/articlio/convertedCorpusPipeline.scala
+++ src/main/scala/com/articlio/convertedCorpusPipeline.scala
@@ -27,7 +27,7 @@ class ConvertedCorpusPipeline {
   def clean(fileText: String) : String = XMLescape(stripArtificialNewLine(fileText))
       
   def toJatsNaive(fileText: String) : String = {
-    val modified  = """<?xml version="1.0" encoding="UTF-8"?><article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML"><body><sec sec-type="intro" id="s1"><title>Introduction</title><p>"""  + fileText +  "</p></sec></body></article>"
+    val modified  = """<?xml version="1.0" encoding="UTF-8"?><article xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML"><body><sec sec-type="introduction" id="s1"><title>Introduction</title><p>"""  + fileText +  "</p></sec></body></article>"
    return modified
   }
   
